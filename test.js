f3db0e60c348da20c27b21c623a434c48632f232
Nihar Annam <32647525+rahinroy@users.noreply.github.com>
2020-11-17 14:01:06 -0600

    new design

---
+++ test.js
@@ -0,0 +1,63 @@
+$(function(){
+    var about = false;
+    var proj = false;
+    var awar = false;
+
+    $("#bio").click(function(){
+        if (proj || awar){
+            if (proj){
+                $("#projects").click();
+            }
+            if (awar){
+                $("#awards").click();
+            }
+        }
+        if (!about){
+            $("#bio").css('color', '#7bc74d');
+            $(".bioInfo").show("medium");
+            about = true;
+        } else {
+            $("#bio").css('color', '#eeeeee');
+            $(".bioInfo").hide("medium");
+            about = false;
+        }
+    })
+    $("#projects").click(function(){
+        if (about || awar){
+            if (about){
+                $("#bio").click();
+            }
+            if (awar){
+                $("#awards").click();
+            }
+        }
+        if (!proj){
+            $("#projects").css('color', '#bc6ff1');
+            $(".projInfo").show("medium");
+            proj = true;
+        } else {
+            $("#projects").css('color', '#eeeeee');
+            $(".projInfo").hide("medium");
+            proj = false;
+        }
+    })
+    $("#awards").click(function(){
+        if (about || proj){
+            if (about){
+                $("#bio").click();
+            }
+            if (proj){
+                $("#projects").click();
+            }
+        }
+        if (!awar){
+            $("#awards").css('color', '#ffa45b');
+            $(".awarInfo").show("medium");
+            awar = true;
+        } else {
+            $("#awards").css('color', '#eeeeee');
+            $(".awarInfo").hide("medium");
+            awar = false;
+        }
+    })
+});

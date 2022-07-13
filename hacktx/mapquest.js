2602716436c2b70c347265e18ac91d7a506afba4
Nihar Annam <32647525+rahinroy@users.noreply.github.com>
2022-07-13 00:33:53 -0400

    new resume

--- hacktx/mapquest.js
+++ hacktx/mapquest.js
@@ -1,11 +1,21 @@
 $( document ).ready(function() {
-  var settings = {
-    "url": "https://maps.googleapis.com/maps/api/directions/json?key=AIzaSyCVoTQAvf9FZZOmn02sGe6CnQ5IjxcaoWY&origin=30.29111, -97.743464&destination=2317 Speedway, Austin, TX 78712&mode=walking",
-    "method": "GET",
-    "timeout": 0,
-  };
+  // var settings = {
+  //   "url": "https://maps.googleapis.com/maps/api/directions/json?key=AIzaSyCVoTQAvf9FZZOmn02sGe6CnQ5IjxcaoWY&origin=30.29111, -97.743464&destination=2317 Speedway, Austin, TX 78712&mode=walking",
+  //   "method": "GET",
+  //   "timeout": 0,
+  // };
   
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+  // $.ajax(settings).done(function (response) {
+  //   console.log(response);
+  // });
 });
+
+
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
